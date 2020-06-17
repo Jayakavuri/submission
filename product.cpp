c317eb2a0b4c866f24a7957fd73ff4de41728b80
Jayakavuri <65058951+Jayakavuri@users.noreply.github.com>
2020-06-17 10:16:51 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c;
+	cout<<"Enter the numbers to find the product\n";
+	cin>>a>>b;
+	c=a*b;
+	cout<<"Product="<<c;
+	return 0;
+}
---
+++ size.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Size of char:"<<sizeof(char);
+	cout<<"\nSize of int:"<<sizeof(int);
+	cout<<"\nSize of float:"<<sizeof(float);
+	cout<<"\nSize of double:"<<sizeof(double);
+	
+}
